d8199a594006d61d4aa77884b52d542e9a7ee3dc
topty <topty@live.com>
2024-08-03 11:09:08 +0700

    fix: Discord ids from emails

---
+++ test/integration-test/accounts/profile.service.integration.spec.ts
@@ -0,0 +1,46 @@
+import { TypegooseModule } from '@m8a/nestjs-typegoose'
+import { INestApplication } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { Test, TestingModule } from '@nestjs/testing'
+import { ProfileService } from 'src/accounts/profile/profile.service'
+import { configuration } from 'src/configs/configuration'
+import { TypegooseConfigBuilderService } from 'src/configs/typegoose.config'
+import { MongoConnection } from 'src/enums/mongo-connection.enum'
+import { ProfileModel } from 'src/models/accounts/profile.model'
+
+describe('Profile service', () => {
+  let app: INestApplication
+  let profileService: ProfileService
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [configuration],
+        }),
+        TypegooseModule.forRootAsync(TypegooseConfigBuilderService.build(MongoConnection.Accounts)),
+        TypegooseModule.forFeature([ProfileModel], MongoConnection.Accounts),
+      ],
+      providers: [ProfileService],
+    }).compile()
+
+    app = module.createNestApplication()
+    await app.init()
+
+    profileService = module.get(ProfileService)
+  })
+
+  it('should be defined', () => {
+    expect(profileService).toBeDefined()
+  })
+
+  it('should return discord ids from emails correctly', async () => {
+    const result = await profileService.discordIdsFromEmails(['nattawatj57@nu.ac.th', 'silas61@nu.ac.th'])
+    expect(result).toEqual([expect.any(String), expect.any(String)])
+  })
+
+  afterAll(() => {
+    return app.close()
+  })
+})
