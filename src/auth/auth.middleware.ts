75af599adde5c65d022f08abbe5f7320cfff2fac
Nattawat Jamlongrad <topty@live.com>
2024-08-11 11:45:37 +0000

    fix(hotfix): AuthMiddleware fallback on failure

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,6 +1,6 @@
-import { ForbiddenException, Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common'
+import { ForbiddenException, Injectable, Logger, NestMiddleware, UnauthorizedException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { VerifyAccessToken } from '@nudchannel/auth'
+import { User, VerifyAccessToken } from '@nudchannel/auth'
 import { IUserOptions } from '@nudchannel/auth/lib/user/user-options.interface'
 import { NextFunction, Response } from 'express'
 import { AccessTokenService } from 'src/accounts/access-token/access-token.service'
@@ -11,6 +11,8 @@ import { Request } from './request.interface'
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(AuthMiddleware.name)
+
   private readonly verifyAccessToken: VerifyAccessToken
 
   private readonly cookieToken: CookieToken
@@ -35,16 +37,24 @@ export class AuthMiddleware implements NestMiddleware {
   }
 
   async use(req: Request, res: Response, next: NextFunction) {
-    req.user = await this.verifyAccessToken.userFromHeaders(req.headers, this.userOptions)
-    if (req.user.isSignedIn()) {
-      return next()
+    try {
+      req.user = await this.verifyAccessToken.userFromHeaders(req.headers, this.userOptions)
+      if (req.user.isSignedIn()) {
+        return next()
+      }
+      const cookieToken = this.cookieToken.fromHttpRequest(req)
+      const accessToken = await cookieToken.getUpdatedAccessToken(res)
+      if (!accessToken) {
+        return next()
+      }
+      req.user = await this.verifyAccessToken
+        .fromAccessToken(accessToken, this.userOptions)
+        .catch(() => new User(undefined, this.userOptions))
+      next()
+    } catch (err) {
+      this.logger.error(`Failed to set User request context.`, err)
+      req.user = new User(undefined, this.userOptions)
+      next()
     }
-    const cookieToken = this.cookieToken.fromHttpRequest(req)
-    const accessToken = await cookieToken.getUpdatedAccessToken(res)
-    if (!accessToken) {
-      return next()
-    }
-    req.user = await this.verifyAccessToken.fromAccessToken(accessToken, this.userOptions)
-    next()
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,6 +17,15 @@ describe('App', () => {
     return request(app.getHttpServer()).get('/bullboard/').expect(HttpStatus.UNAUTHORIZED)
   })
 
+  it('GET /bullboard (Expired token)', async () => {
+    const cookies = TestData.aValidSupertestCookies()
+      .withAccessToken(
+        'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmODU0ZWJmYzJkMTIzYjQ2NzE4MzA0MCIsImdyb3VwcyI6WyJudWRjaCIsIm51ZGNoXzIwMjIiLCJudWRjaF8yMDIxIiwiYWRtaW4iLCJpdCJdLCJuYW1lIjoiRGV2ZWxvcG1lbnQgVXNlciIsInBob3RvIjoiaHR0cHM6Ly93d3cubnVkY2hhbm5lbC5jb20vY292ZXIucG5nIiwiaWF0IjoxNzIzMzc0Nzg1LCJleHAiOjE3MjMzNzQ3ODUsImlzcyI6ImFjY291bnRzLm51ZGNoYW5uZWwuY29tIiwic3ViIjoiNWY4NTRlYmZjMmQxMjNiNDY3MTgzMDQwIn0.QkGY_uRNz5ISSd6DidI-EqVuCdaG3Mq1DeClJ8Vt2Fwy6cIRPYKjjB2RkDfo66M6a7PMBsKJ-4QPK9c_5yEHbA7vFnuJoO6a7n-AaAc87cDANOIgoJ5WKokpPtmjgfgoPemVvNBVXyaah0j7QE5c23ktT9Pk20uRp3YJSjXgDmmz0pYOrrhlcLT4ymb_ajuca0V4obj81bixvwmYDD3LiIERhpGJYeX-B0N-TfF0qCcZmQgZlciOaOOOwJhZ9NYjNjX_4NyDQ6JyVkbnTX5ciHyxG3NZMnfoYdQDtRWawcfb1HUdbCKkqFpUwUIHGKxkF9uuOpCEldr0Qxw9yvlYuA',
+      )
+      .build()
+    return await request(app.getHttpServer()).get('/bullboard/').set('Cookie', cookies).expect(HttpStatus.UNAUTHORIZED)
+  })
+
   it('GET /bullboard (Forbidden)', async () => {
     const accessToken = await TestData.aValidAccessToken().build()
     const cookies = TestData.aValidSupertestCookies().withAccessToken(accessToken).build()
