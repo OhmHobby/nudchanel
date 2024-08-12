2ce56e602e9a2ef491e766ff5c412657cb35099e
topty <topty@live.com>
2024-08-12 15:09:44 +0700

    fix: Missing migration processor

--- src/migration/migration.worker.module.ts
+++ src/migration/migration.worker.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AccountsWorkerModule } from 'src/accounts/accounts.worker.module'
+import { MigrationProcessorService } from './migration-processor.service'
 import { MigrationController } from './migration.controller.service'
 
 @Module({
   imports: [AccountsWorkerModule],
   controllers: [MigrationController],
-  providers: [],
+  providers: [MigrationProcessorService],
 })
 export class MigrationWorkerModule {}
