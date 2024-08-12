6a919aace5b4dfdd5311b14592fda8214867b790
topty <topty@live.com>
2024-08-12 16:34:06 +0700

    Revert "without bulk"
    This reverts commit a02807c0bb823ea34cc16308971ca093edb70e70.

--- src/accounts/discord/discord-processor.service.ts
+++ src/accounts/discord/discord-processor.service.ts
@@ -25,15 +25,14 @@ export class DiscordProcessorService {
   ) {}
 
   async triggerProfileSyncAll() {
-    const delayFactorMs = 500
+    const delayFactorMs = 1000
     const discordIds = await this.profileService.findAllDiscordIds()
-    this.logger.log({ message: 'triggerProfileSyncAll', discordIds })
-    await Promise.all(
-      discordIds.map((discordId, i) =>
-        this.migrationQueue.add(BullJobName.MigrateDiscordProfileSync, discordId, {
-          delay: delayFactorMs * i,
-        }),
-      ),
+    await this.migrationQueue.addBulk(
+      discordIds.map((discordId, i) => ({
+        name: BullJobName.MigrateDiscordProfileSync,
+        data: discordId,
+        opts: { delay: delayFactorMs * i },
+      })),
     )
     return discordIds
   }
