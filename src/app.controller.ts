6beb7a20feacb65f9b07dbb45c7f53ab99169d21
Pheranat Lorwongkamol <pheranatlor@gmail.com>
2024-11-09 16:03:57 +0000

    feat: add-baseurl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get, Header } from '@nestjs/common'
 import { SkipHttpLogging } from './helpers/skip-http-logging.decorator'
+import { ConfigService } from '@nestjs/config'
+const BASEURL = 'http.baseUrl'
 
 @Controller()
 export class AppController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get('/ping')
   @Header('content-type', 'text/plain')
   @SkipHttpLogging()
@@ -14,5 +18,11 @@ export class AppController {
   @Header('content-type', 'text/plain')
   getVersion() {
     return process.env.npm_package_version
+  }
+
+  @Get('/baseurl')
+  @Header('content-type', 'text/plain')
+  getBaseUrl() {
+    return this.configService.get(BASEURL)
   }
 }
