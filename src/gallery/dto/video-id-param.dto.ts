ff47da8a1cfadf9e168ca2e3f3ecb1e9bac3a322
topty <topty@live.com>
2025-01-19 15:43:57 +0700

    fix build

--- src/gallery/dto/video-id-param.dto.ts
+++ src/gallery/dto/video-id-param.dto.ts
@@ -1,6 +1,16 @@
+import { BadRequestException } from '@nestjs/common'
 import { ApiProperty } from '@nestjs/swagger'
+import { Transform } from 'class-transformer'
+import { Types } from 'mongoose'
 
 export class VideoIdParamDto {
   @ApiProperty({ type: String })
-  videoId: string
+  @Transform(({ value }) => {
+    try {
+      return new Types.ObjectId(value as string)
+    } catch (err) {
+      throw new BadRequestException('VideoId must be an ObjectId')
+    }
+  })
+  videoId: Types.ObjectId
 }
