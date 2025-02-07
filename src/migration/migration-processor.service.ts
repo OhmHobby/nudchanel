98113f767eb9c67ebc846670442599c0c1ce94ac
topty <topty@live.com>
2025-02-07 23:34:29 +0700

    fix: Clean up google creds

--- src/migration/migration-processor.service.ts
+++ src/migration/migration-processor.service.ts
@@ -1,22 +1,16 @@
 import { InjectModel } from '@m8a/nestjs-typegoose'
 import { Process, Processor } from '@nestjs/bull'
 import { Injectable, Logger } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { InjectDataSource } from '@nestjs/typeorm'
 import { ReturnModelType } from '@typegoose/typegoose'
 import { Job } from 'bull'
 import { join } from 'path'
 import { ProfilePhotoService } from 'src/accounts/profile/profile-photo.service'
-import { ApplicationSettingService } from 'src/application-setting/application-setting.service'
-import { DataMigrationEntity } from 'src/entities/data-migration.entity'
 import { BullJobName } from 'src/enums/bull-job-name.enum'
 import { BullQueueName } from 'src/enums/bull-queue-name.enum'
-import { Config } from 'src/enums/config.enum'
 import { DataMigration } from 'src/enums/data-migration.enum'
 import { Orientation } from 'src/enums/orientation.enum'
 import { PhotoSize } from 'src/enums/photo-size.enum'
 import { ProfileModel } from 'src/models/accounts/profile.model'
-import { GoogleCredentialModel } from 'src/models/google-credential.model'
 import { UploadBatchFileModel } from 'src/models/photo/upload-batch-file.model'
 import { PhotoPath } from 'src/photo/models/photo-path.model'
 import { PhotoV1Service } from 'src/photo/photo-v1.service'
@@ -24,7 +18,6 @@ import { PhotoMetadataService } from 'src/photo/processor/photo-metadata.service
 import { PhotoProcessorService } from 'src/photo/processor/photo-processor.service'
 import { UploadTaskRuleWatermark } from 'src/photo/upload-rules/upload-task-rule-watermark'
 import { StorageService } from 'src/storage/storage.service'
-import { DataSource } from 'typeorm'
 
 @Injectable()
 @Processor(BullQueueName.Migration)
@@ -32,21 +25,15 @@ export class MigrationProcessorService {
   private readonly logger = new Logger(MigrationProcessorService.name)
 
   constructor(
-    @InjectDataSource()
-    private readonly dataSource: DataSource,
     @InjectModel(ProfileModel)
     private readonly profileModel: ReturnModelType<typeof ProfileModel>,
     @InjectModel(UploadBatchFileModel)
     private readonly batchFileModel: ReturnModelType<typeof UploadBatchFileModel>,
-    @InjectModel(GoogleCredentialModel)
-    protected readonly googleCredentialModel: ReturnModelType<typeof GoogleCredentialModel>,
-    private readonly configService: ConfigService,
     private readonly profilePhotoService: ProfilePhotoService,
     private readonly storageService: StorageService,
     private readonly photoV1Service: PhotoV1Service,
     private readonly photoProcessor: PhotoProcessorService,
     private readonly photoMetadata: PhotoMetadataService,
-    private readonly applicationSettingService: ApplicationSettingService,
   ) {}
 
   @Process({ name: BullJobName.MigratePhoto, concurrency: 1 })
@@ -99,16 +86,8 @@ export class MigrationProcessorService {
   }
 
   @Process({ name: BullJobName.MigrateData, concurrency: 0 })
-  async migrateData({ data: name }: Job<DataMigration>) {
+  migrateData({ data: name }: Job<DataMigration>) {
     try {
-      if (name === DataMigration.GoogleCredential) {
-        const email = this.configService.getOrThrow(Config.GAPIS_EMAIL)
-        const { token } = await this.googleCredentialModel.findById(email).orFail().exec()
-        await this.applicationSettingService.setGoogleCredential(JSON.stringify(token))
-        await this.dataSource
-          .getRepository(DataMigrationEntity)
-          .save(new DataMigrationEntity({ id: DataMigration.GoogleCredential }))
-      }
       throw new Error(`${name} not found`)
     } catch (err) {
       this.logger.error(err)
