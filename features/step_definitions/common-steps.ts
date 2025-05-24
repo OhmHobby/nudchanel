89bc74d0608050f860f80f6daa4ab51b79276894
Nattawat Jamlongrad <topty@live.com>
2025-05-24 07:35:21 +0000

    chore(deps): Supertest 7.1.1

--- features/step_definitions/common-steps.ts
+++ features/step_definitions/common-steps.ts
@@ -9,9 +9,8 @@ import { Workspace } from './workspace'
 export class CommonSteps {
   constructor(private readonly _workspace: Workspace) {}
 
-  async httpRequest(method: string, endpoint?: string, query?: object, body?: object, serverUrl?: string) {
+  async httpRequest(method: string, endpoint?: string, query: object = {}, body?: object, serverUrl?: string) {
     const test = agent(serverUrl ?? this._workspace.serverUrl)
-    if (query) test.query(query)
     if (this._workspace.user.id) {
       const privateKey = config.get<string>(Config.NUDCH_TOKEN_PRIVATE_KEY)
       const issuer = config.get<string>(Config.NUDCH_TOKEN_ISSUER)
@@ -25,7 +24,7 @@ export class CommonSteps {
     Object.entries(this._workspace.requestHeaders).map(([k, v]) => test.set(k, v))
     for (let attempts = 3; attempts; attempts--) {
       if (method === 'GET') {
-        const getTest = test.get(requestEndpoint)
+        const getTest = test.get(requestEndpoint).query(query)
         if (this._workspace.responseType) getTest.responseType(this._workspace.responseType)
         this._workspace.response = await getTest.send()
       } else if (method === 'POST') {
@@ -35,9 +34,9 @@ export class CommonSteps {
           ? await request.attach(attach.name, attach.file, attach.filename)
           : await request.send(body)
       } else if (method === 'PUT') {
-        this._workspace.response = await test.put(requestEndpoint).send(body)
+        this._workspace.response = await test.put(requestEndpoint).query(query).send(body)
       } else if (method === 'PATCH') {
-        this._workspace.response = await test.patch(requestEndpoint).send(body)
+        this._workspace.response = await test.patch(requestEndpoint).query(query).send(body)
       } else {
         throw new Error('Unknown method ' + method)
       }
