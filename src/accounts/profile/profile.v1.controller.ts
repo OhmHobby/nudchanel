7e0929c29007756683f6b24de37c1935c8eb4722
topty <topty@live.com>
2025-06-12 21:23:33 +0700

    feat(account): Update profile name

--- src/accounts/profile/profile.v1.controller.ts
+++ src/accounts/profile/profile.v1.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Logger, Param, Put } from '@nestjs/common'
-import { ApiBearerAuth, ApiCookieAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger'
+import { ApiBearerAuth, ApiCookieAuth, ApiNoContentResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
 import { User } from '@nudchannel/auth'
 import { Types } from 'mongoose'
 import { AuditLog } from 'src/audit-log/audit-log.decorator'
@@ -7,8 +7,10 @@ import { AuthGroups } from 'src/auth/auth-group.decorator'
 import { UserCtx } from 'src/auth/user.decorator'
 import { ImportProfilePhotoDto } from '../dto/import-profile-photo.dto'
 import { ProfileIdDto } from '../dto/profile-id.dto'
+import { UpdateProfileNameDto } from '../dto/update-profile-name.dto'
 import { ProfilePhotoResponseModel } from '../models/profile-photo.response-model'
 import { ProfileResponseModel } from '../models/profile.response.model'
+import { ProfileNameService } from './profile-name.service'
 import { ProfilePhotoService } from './profile-photo.service'
 import { ProfileService } from './profile.service'
 
@@ -19,6 +21,7 @@ export class ProfileV1Controller {
 
   constructor(
     private readonly profileService: ProfileService,
+    private readonly profileNameService: ProfileNameService,
     private readonly profilePhotoService: ProfilePhotoService,
   ) {}
 
@@ -31,6 +34,32 @@ export class ProfileV1Controller {
   async getMyProfile(@UserCtx() user: User): Promise<ProfileResponseModel> {
     const doc = await this.profileService.findByIdPopulated(new Types.ObjectId(user.id!))
     return ProfileResponseModel.fromModel(doc!)
+  }
+
+  @Put('me/name')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiNoContentResponse()
+  @ApiBearerAuth()
+  @ApiCookieAuth()
+  async updateMyProfileName(@Body() dto: UpdateProfileNameDto, @UserCtx() user: User): Promise<void> {
+    const profileId = new Types.ObjectId(user.id!)
+    await Promise.all([
+      this.profileNameService.upsert('en', profileId, dto.name.toModel()),
+      this.profileNameService.upsert('th', profileId, dto.localName.toModel()),
+    ])
+  }
+
+  @Put(':profileId/name')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiNoContentResponse()
+  @ApiBearerAuth()
+  @ApiCookieAuth()
+  @AuthGroups('head')
+  async updateProfileName(@Param() { profileId }: ProfileIdDto, @Body() dto: UpdateProfileNameDto): Promise<void> {
+    await Promise.all([
+      this.profileNameService.upsert('en', profileId.objectId, dto.name.toModel()),
+      this.profileNameService.upsert('th', profileId.objectId, dto.localName.toModel()),
+    ])
   }
 
   @Put(':profileId/photos')
