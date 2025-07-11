25f6377ca59693d78206ae8df22257925088181b
topty <topty@live.com>
2025-07-11 13:08:23 +0700

    unify it instead

--- src/recruit/context/recruit-context.model.ts
+++ src/recruit/context/recruit-context.model.ts
@@ -40,8 +40,4 @@ export class RecruitContext {
     if (this.applicantId) return this.applicantId
     else throw new ForbiddenException('The applicant has not been created')
   }
-
-  get isModerator(): boolean {
-    return !!this.manageableRecruitId.length
-  }
 }
