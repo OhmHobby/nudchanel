d4c0606e5d163af9ccbfac3659efbbf48d71917d
topty <topty@live.com>
2025-07-12 10:56:10 +0700

    fix(accounts): Use mfa required configuration

--- src/configs/configuration.ts
+++ src/configs/configuration.ts
@@ -1,4 +1,3 @@
 import config from 'config'
-import { Config } from 'src/enums/config.enum'
 
-export const configuration = () => ({ ...config, [Config.IS_PROD_ENV]: process.env.NODE_ENV === 'production' })
+export const configuration = () => ({ ...config })
