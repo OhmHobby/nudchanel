1b204435a4fee4b6a90aef96a8969ef0c8e760a0
mrtbts <66320961+scratchusernamemrtbts@users.noreply.github.com>
2025-07-16 09:35:33 +0700

    idk maybe this

--- src/entities/recruit/recruit-applicant-role.entity.ts
+++ src/entities/recruit/recruit-applicant-role.entity.ts
@@ -63,7 +63,7 @@ export class RecruitApplicantRoleEntity extends BaseEntity {
       return this.offerAccepted ? RecruitOfferResponseEnum.accepted : RecruitOfferResponseEnum.declined
     } else if (
       this.offerAccepted === false &&
-      ((this.offerExpireAt && now.getTime() >= this.offerExpireAt.getTime()) || this.offerResponseAt)
+      ((this.offerExpireAt && now.getTime() <= this.offerExpireAt.getTime()) || this.offerResponseAt)
     ) {
       return this.offerExpireAt ? RecruitOfferResponseEnum.pending : RecruitOfferResponseEnum.rejected
     } else return RecruitOfferResponseEnum.tba
